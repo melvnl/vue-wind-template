ae7d54fe230eee2cb1e6061e127d266bf019a01c
melvnl <melvinlim2000@gmail.com>
2021-01-06 00:00:22 +0700

    First Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+      <!-- component -->
+
+<footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+    <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
+        <!-- Col-1 -->
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <!-- Col Title -->
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Getting Started
+            </div>
+
+            <!-- Links -->
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Installation
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Release Notes
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Upgrade Guide
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Using with Preprocessors
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Optimizing for Production
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Browser Support
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                IntelliSense
+            </a>
+        </div>
+
+        <!-- Col-2 -->
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <!-- Col Title -->
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Core Concepts
+            </div>
+
+            <!-- Links -->
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Utility-First
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Responsive Design
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Hover, Focus, & Other States
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Dark Mode
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Adding Base Styles
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Extracting Components
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Adding New Utilities
+            </a>
+        </div>
+
+        <!-- Col-3 -->
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <!-- Col Title -->
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Customization
+            </div>
+
+            <!-- Links -->
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Configuration
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Theme Configuration
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Breakpoints
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Customizing Colors
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Customizing Spacing
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Configuring Variants
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Plugins
+            </a>
+        </div>
+
+        <!-- Col-3 -->
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <!-- Col Title -->
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Community
+            </div>
+
+            <!-- Links -->
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                GitHub
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Discord
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                Twitter
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-20">
+                YouTube
+            </a>
+        </div>
+    </div>
+
+    <!-- Copyright Bar -->
+    <div class="pt-2">
+        <div class="flex pb-5 px-3 m-auto pt-5 
+            border-t border-gray-500 text-gray-400 text-sm 
+            flex-col md:flex-row max-w-6xl">
+            <div class="m-auto mt-2 ">
+                © Copyright 2020. All Rights Reserved.
+            </div>
+        </div>
+    </div>
+</footer>
+  </div>
+</template>
+
+<script>
+export default {
+name : 'Footer'
+}
+</script>
+
+<style>
+
+</style>
